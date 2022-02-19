6c015deba678d7c1d8619487a7ee08b70627fa22
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-02-19 14:08:45 +0000

    basic configuration and token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract Token is ERC20{
+
+  constructor(uint256 _totalSupply) ERC20("RACToken","RAC") { 
+    _mint(msg.sender,_totalSupply);
+  }
+
+}
