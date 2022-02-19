6fe38c20acf20bd43d71a2d8a396880e3a3245e6
Oscar Lugo <oscaremiliolugo@gmail.com>
2022-02-19 17:03:53 +0000

    integrate make offers

--- contracts/Nft/DropNfts.sol
+++ contracts/Nft/DropNfts.sol
@@ -18,6 +18,7 @@ contract NftDrop {
 		string hash;
 		uint256 nftId;
 		uint256 initialPrice;
+		uint256 totalOffers;
 		Offer higgestOffer;
 	}
 	event CreateDrop(uint256 indexed dropId, uint256 indexed duration);
@@ -29,7 +30,8 @@ contract NftDrop {
 	function createDrop(
 		string[] calldata _hashArray,
 		string calldata _name,
-		uint256 _duration
+		uint256 _duration,
+		uint256 _initialPrice
 	) external returns (uint256) {
 		for (uint256 i = 0; i < _hashArray.length; i++) {
 			require(!nftContract.existHash(_hashArray[i]), "hash already exist");
@@ -38,10 +40,21 @@ contract NftDrop {
 		newDrop.avaliableHashes = _hashArray;
 		newDrop.duration = _duration;
 		newDrop.name = _name;
+		newDrop.initialPrice = _initialPrice;
 		drops[totalDrops] = newDrop;
 		emit CreateDrop(totalDrops, _duration);
 
 		totalDrops++;
 		return totalDrops - 1;
+	}
+
+	function makeOffer(uint256 _dropId, uint256 _price) external {
+		require(
+			_price > drops[_dropId].initialPrice &&
+				_price > drops[_dropId].higgestOffer.price,
+			"Your offer should be bigger than the actual price"
+		);
+		drops[_dropId].higgestOffer.owner = msg.sender;
+		drops[_dropId].higgestOffer.price = _price;
 	}
 }
--- test/NftDrop.js
+++ test/NftDrop.js
@@ -15,28 +15,71 @@ describe("NftDrop", () => {
 			hashesArray = ["111", "222"];
 			duration = 60 * 10;
 			dropName = "first drop";
+			initialPrice = 10;
 			await nft.mint("123", "hola");
 		});
 
 		it("fail creating, a hash exist like a nft", async () => {
 			await expect(
-				drop.createDrop([...hashesArray, "123"], dropName, duration)
+				drop.createDrop(
+					[...hashesArray, "123"],
+					dropName,
+					duration,
+					initialPrice
+				)
 			).to.be.revertedWith("hash already exist");
 		});
 		it("create drop", async () => {
-			const tx = await drop.createDrop(hashesArray, dropName, duration);
+			const tx = await drop.createDrop(
+				hashesArray,
+				dropName,
+				duration,
+				initialPrice
+			);
 			await printGas(tx);
 			const dropId = Number(tx.value);
 			const newDrop = await drop.drops(dropId);
 			expect(newDrop.name).to.be.equal(dropName);
 			expect(newDrop.duration).to.be.equal(duration);
 			expect(newDrop.hash).to.be.equal("");
+			expect(newDrop.initialPrice).to.be.equal(10);
 		});
 
 		it("event emmited", async () => {
-			await expect(drop.createDrop(hashesArray, dropName, duration))
+			await expect(
+				drop.createDrop(hashesArray, dropName, duration, initialPrice)
+			)
 				.to.emit(drop, "CreateDrop")
 				.withArgs(0, duration);
+		});
+	});
+	describe("make offers", () => {
+		beforeEach(async () => {
+			hashesArray = ["111", "222"];
+			duration = 60 * 10;
+			dropName = "first drop";
+			await drop.createDrop(hashesArray, dropName, duration, 10);
+		});
+		it("fail offer are less than the initial price", async () => {
+			await expect(drop.makeOffer(0, 9)).to.be.revertedWith(
+				"Your offer should be bigger than the actual price"
+			);
+		});
+
+		it("create offer", async () => {
+			const tx = await drop.makeOffer(0, 11);
+			await printGas(tx);
+			const actualDrop = await drop.drops(0);
+			expect(actualDrop.higgestOffer.owner).to.be.equal(deployer);
+			expect(actualDrop.higgestOffer.price).to.be.equal(11);
+		});
+
+		it("fail offer less than the higgest offer", async () => {
+			await drop.makeOffer(0, 11);
+			await printGas(tx);
+			await expect(drop.makeOffer(0, 11)).to.be.revertedWith(
+				"Your offer should be bigger than the actual price"
+			);
 		});
 	});
 });
